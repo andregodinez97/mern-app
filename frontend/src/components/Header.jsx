a464dd0cdfd400c0e969a47bd06f12dde39ac618
andregodinez97 <60364727+andregodinez97@users.noreply.github.com>
2022-10-07 21:18:43 +0100

    initial commit for frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='logo'>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
